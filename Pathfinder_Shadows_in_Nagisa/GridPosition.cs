187d20d6f7a1d447f23f2f3fd7024086ec2e8067
Trenton Scott <InsaneGriot@Outlook.com>
2025-04-01 22:00:53 -0400

    Grid System

---
+++ Pathfinder_Shadows_in_Nagisa/GridPosition.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace PathfinderShadowsInNagisa
+{
+    public struct GridPosition
+    {
+        public int x;
+        public int z;
+
+        public GridPosition(int x, int z)
+        {
+            this.x = x;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"x: {x}; z: {z}";
+        }
+    }
+}
---
+++ Pathfinder_Shadows_in_Nagisa/GridSystem.cs
@@ -0,0 +1,41 @@
+﻿using Stride.Core.Mathematics;
+using System;
+
+namespace PathfinderShadowsInNagisa
+{
+    public class GridSystem
+    {
+        private int width;
+        private int length;
+        private float cellSize;
+        public GridSystem(int width, int length, float cellSize)
+        {
+            this.width = width;
+            this.length = length;
+            this.cellSize = cellSize;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+
+                }
+            }
+        }
+
+        public Vector3 GetWorldPosition(int x, int z)
+        {
+            return new Vector3(x, 0, z) * cellSize;
+        }
+
+        public GridPosition GetGridPosition(Vector3 worldPosition)
+        {
+            return new GridPosition(
+                Convert.ToInt32(worldPosition.X / cellSize),
+                Convert.ToInt32(worldPosition.Z / cellSize)
+                );
+        }
+    }
+
+
+}
