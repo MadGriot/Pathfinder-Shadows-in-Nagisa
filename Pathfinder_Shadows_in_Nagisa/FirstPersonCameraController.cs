7ffab5d67c5a1b96527d5e060f93ebe005708f7a
Trenton Scott <InsaneGriot@Outlook.com>
2025-04-05 01:47:18 -0400

    Reset?

--- Pathfinder_Shadows_in_Nagisa/FirstPersonCameraController.cs
+++ Pathfinder_Shadows_in_Nagisa/FirstPersonCameraController.cs
@@ -1,4 +1,9 @@
-﻿using Stride.Core.Mathematics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
 
