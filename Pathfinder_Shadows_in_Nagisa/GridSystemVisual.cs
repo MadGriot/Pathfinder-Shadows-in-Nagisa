2cecb15184019c006319e84bfe0c43943bd0b20c
Trenton Scott <InsaneGriot@Outlook.com>
2025-04-06 05:02:11 -0400

    Grid Visual Start

---
+++ Pathfinder_Shadows_in_Nagisa/GridSystemVisual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace PathfinderShadowsInNagisa
+{
+    public class GridSystemVisual : SyncScript
+    {
+        public Prefab GridSystemVisualSinglePrefab;
+
+        public override void Start()
+        {
+            // Initialization of the script.
+            var cell = GridSystemVisualSinglePrefab.Instantiate();
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+    }
+}
