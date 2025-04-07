0e56251757d86b9121641e66246abc84c82d43d7
Trenton Scott <InsaneGriot@Outlook.com>
2025-04-07 11:06:04 -0400

    GridSystemVisual Update

--- Pathfinder_Shadows_in_Nagisa/GridSystemVisual.cs
+++ Pathfinder_Shadows_in_Nagisa/GridSystemVisual.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Stride.Core.Mathematics;
-using Stride.Input;
 using Stride.Engine;
 
 namespace PathfinderShadowsInNagisa
@@ -13,21 +8,48 @@ namespace PathfinderShadowsInNagisa
     {
         public Prefab GridSystemVisualSinglePrefab;
 
+        private GridSystemVisualSingle[,] gridSystemVisualSingleArray;
+
         public override void Start()
         {
-            // Initialization of the script.
+            gridSystemVisualSingleArray = new GridSystemVisualSingle[
+                LevelGrid.Instance.GetWidth(),
+                LevelGrid.Instance.GetLength()];
             for (int x = 0; x < LevelGrid.Instance.GetWidth(); x++)
             {
                 for (int z = 0; z < LevelGrid.Instance.GetLength(); z++)
                 {
-                    Entity cell = GridSystemVisualSinglePrefab.Instantiate().First();
-                    cell.Transform.Position = LevelGrid.Instance.GetWorldPositionWithY(new GridPosition(x, z), cell.Transform.Position.Y);
-                    Entity.Scene.Entities.Add(cell);
+                    Entity gridSystemVisualSingleTransform 
+                        = GridSystemVisualSinglePrefab.Instantiate().First();
+                    gridSystemVisualSingleTransform.Transform.Position = LevelGrid.Instance
+                        .GetWorldPositionWithY(new GridPosition(x, z), gridSystemVisualSingleTransform.Transform.Position.Y);
+                    Entity.Scene.Entities.Add(gridSystemVisualSingleTransform);
 
+                    gridSystemVisualSingleArray[x, z] = gridSystemVisualSingleTransform.Get<GridSystemVisualSingle>();
+
+
+                }
+            }
+            HideAllGridPositions();
+        }
+        public void HideAllGridPositions()
+        {
+            for (int x = 0; x < LevelGrid.Instance.GetWidth(); x++)
+            {
+                for (int z = 0; z < LevelGrid.Instance.GetLength(); z++)
+                {
+                    gridSystemVisualSingleArray[x, z].Hide();
                 }
             }
         }
 
+        public void ShowGridPositionList(List<GridPosition> gridPositionList)
+        {
+            foreach (GridPosition gridPosition in gridPositionList)
+            {
+                gridSystemVisualSingleArray[gridPosition.x, gridPosition.z].Show();
+            }
+        }
         public override void Update()
         {
             // Do stuff every new frame
---
+++ Pathfinder_Shadows_in_Nagisa/GridSystemVisualSingle.cs
@@ -0,0 +1,31 @@
+﻿using Stride.Engine;
+using Stride.Rendering;
+
+namespace PathfinderShadowsInNagisa
+{
+    public class GridSystemVisualSingle : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+        public Material blankMaterial;
+        public Material material;
+        public Entity cell;
+
+
+        public void Show()
+        {
+            cell.Get<ModelComponent>().Materials.Remove(0);
+            cell.Get<ModelComponent>().Materials.Add(0, material);
+        }
+
+        public void Hide()
+        {
+            cell.Get<ModelComponent>().Materials.Remove(0);
+            cell.Get<ModelComponent>().Materials.Add(0, blankMaterial);
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+    }
+}
